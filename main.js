c9039a473000e5cef2529f4fe884d5de025c233f
Dahang Xu <xudahang@gmail.com>
2017-04-06 12:21:43 -0400

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+var Index = require("./routes/index");
+var path = require("path");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', Index.router);
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/dist', express.static(__dirname + '/dist'));
+app.use('/fontawesome-webfont.woff2', express.static(__dirname + '/dist/fontawesome-webfont.woff2'));
+app.listen(3000, function () {
+    console.log("listening on port 3000");
+});
